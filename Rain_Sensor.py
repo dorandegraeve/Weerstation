f57c80b25304ebeb3861ab6d867bbd23a4bdc661
Doran <doran.degraeve@student.howest.be>
2017-06-14 12:56:26 +0200

    Rain Sensor sample

---
+++ Rain_Sensor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from time import sleep
+import os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+state = GPIO.input(23)
+
+if (state == 0):
+    print "Water detected!"
+else:
+    print "Water not detected"
